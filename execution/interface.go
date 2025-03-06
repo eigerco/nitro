f142ca69905b9ce167b3f645853071fa750a4b8b
Diego Ximenes <dxmendes1@gmail.com>
2025-03-06 17:10:59 -0300

    ConsensusInfo.SyncTargetMessageCount returns PromiseInterface

--- execution/interface.go
+++ execution/interface.go
@@ -87,7 +87,7 @@ type BatchFetcher interface {
 type ConsensusInfo interface {
 	Synced() containers.PromiseInterface[bool]
 	FullSyncProgressMap() containers.PromiseInterface[map[string]interface{}]
-	SyncTargetMessageCount() arbutil.MessageIndex
+	SyncTargetMessageCount() containers.PromiseInterface[arbutil.MessageIndex]
 	BlockMetadataAtCount(count arbutil.MessageIndex) containers.PromiseInterface[common.BlockMetadata]
 }
 
